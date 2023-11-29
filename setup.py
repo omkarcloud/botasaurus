def62d4056116368aa29004ef1357b11cbb142cd
Chetan11-Dev <chetansirsa@gmail.com>
2023-11-29 12:00:59 +0530

    fixes

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def get_description():
 setup(
     name='botasaurus',
     packages=['botasaurus'],
-    version='3.0.2',
+    version='3.0.3',
     license='MIT',
     project_urls={
         "Documentation": "https://omkar.cloud/botasaurus/",
