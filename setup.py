c326a9c7dca3ec245d2cac6cecca151c2475db4f
noreply <noreply@users.noreply.github.com>
2024-02-03 17:59:48 +0530

    fixes

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ pre_install()
 setup(
     name="botasaurus",
     packages=["botasaurus"],
-    version="3.2.9",
+    version='3.2.13',
     license="MIT",
     project_urls={
         "Documentation": "https://omkar.cloud/botasaurus/",
