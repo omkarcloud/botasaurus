3d552e0d04edd846123994c2ff579a4571d022c6
Chetan <53407137+Chetan11-dev@users.noreply.github.com>
2024-03-18 14:55:30 +0530

    fixes

--- botasaurus_server/botasaurus_server/db_setup.py
+++ botasaurus_server/botasaurus_server/db_setup.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .models import Base  # Assuming Task is one of your models
 from os import path, getcwd
+from .config import is_master
+from .models import Base  # Assuming Task is one of your models
 
 
 def relative_path_backend():
     """Determines the relative path to the database file, prioritizing 'backend/db.sqlite3'."""
+    if is_master:
+        return path.abspath(path.join(getcwd(), "..", "db", "db.sqlite3"))
     return path.abspath(path.join(getcwd(), "db.sqlite3"))
 
 database_path = relative_path_backend()
