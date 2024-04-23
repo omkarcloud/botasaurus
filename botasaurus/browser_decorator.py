fc05922fa63ab24da1546cd38bd2093d85963d60
Chetan <53407137+Chetan11-dev@users.noreply.github.com>
2024-04-23 16:59:01 +0530

    fixes

---
+++ botasaurus/browser_decorator.py
@@ -0,0 +1,373 @@
+from functools import wraps
+from queue import Queue
+from threading import Thread
+from traceback import print_exc, format_exc
+from typing import Any, Callable, Optional, Union, List
+from time import sleep
+from botasaurus.decorators_common import first_run, evaluate_proxy, write_output, IS_PRODUCTION, AsyncQueueResult, AsyncResult, get_driver_url_safe, run_parallel, save_error_logs
+from .utils import is_errors_instance
+from .list_utils import flatten
+from .dontcache import is_dont_cache
+from botasaurus_driver import Driver
+
+def browser(
+    _func: Optional[Callable] = None,
+    *,
+    parallel: Optional[Union[Callable[[Any], int], int]] = None,
+    data: Optional[Union[Callable[[], Any], Any]] = None,
+    metadata: Optional[Any] = None,
+    cache: Union[bool, str] = False,  
+    block_images: bool = False,
+    block_images_and_css: bool = False,
+    window_size: Optional[Union[Callable[[Any], str], str]] = None,
+    tiny_profile: bool = False,
+    wait_for_complete_page_load: bool = False,
+    add_arguments: Optional[Union[List[str], Callable[[Any, List[str]], None]]] = None,
+    extensions: Optional[Union[List[Any], Callable[[Any], List[Any]]]] = None,
+    lang: Optional[Union[Callable[[Any], str], str]] = None,
+    headless: Optional[Union[Callable[[Any], bool], bool]] = False,
+    beep: bool = False,
+    close_on_crash: bool = False,
+    async_queue: bool = False,
+    run_async: bool = False,
+    profile: Optional[Union[Callable[[Any], str], str]] = None,
+    proxy: Optional[Union[Callable[[Any], str], str]] = None,
+    user_agent: Optional[Union[Callable[[Any], str], str]] = None,
+    reuse_driver: bool = False,
+    keep_drivers_alive: bool = False,
+    output: Optional[Union[str, Callable]] = "default",
+    output_formats: Optional[List[str]] = None,
+    raise_exception: bool = False,
+    must_raise_exceptions: Optional[List[Any]] = None,
+    max_retry: Optional[int] = None,
+    retry_wait: Optional[int] = None,
+    create_error_logs: bool = True,
+    create_driver: Optional[Callable] = None,
+) -> Callable:
+    def decorator_browser(func: Callable) -> Callable:
+        if not hasattr(func, '_scraper_type'):
+            func._scraper_type = "browser"
+
+        def close_driver(driver):
+            try:
+                driver.close()
+            except Exception as e:
+                raise
+
+        def close_driver_pool(pool: list):
+            if len(pool) == 1:
+                close_driver(pool[0])
+                while pool:
+                    pool.pop()
+            elif len(pool) > 0:
+                run_parallel(close_driver, pool, len(pool))
+                while pool:
+                    pool.pop()
+
+        url = None
+
+        @wraps(func)
+        def wrapper_browser(*args, **kwargs) -> Any:
+            global first_run  # Declare the global variable to modify it
+            if first_run:  # Check if it's the first run
+                print("Running")  # If so, print "Running"
+                first_run = False  # Set the flag to False so it doesn't run again
+
+            nonlocal parallel, data, cache, block_images_and_css, block_images, window_size, metadata, add_arguments, extensions
+            nonlocal tiny_profile, wait_for_complete_page_load, lang, headless, beep
+            nonlocal close_on_crash, async_queue, run_async, profile
+            nonlocal proxy, user_agent, reuse_driver, keep_drivers_alive, raise_exception, must_raise_exceptions
+
+            nonlocal output, output_formats, max_retry, retry_wait, create_driver, create_error_logs
+
+            parallel = kwargs.get("parallel", parallel)
+            data = kwargs.get("data", data)
+            cache = kwargs.get("cache", cache)
+            block_images = kwargs.get("block_images", block_images)
+            block_images_and_css = kwargs.get("block_images_and_css", block_images_and_css)
+            add_arguments = kwargs.get("add_arguments", add_arguments)
+            extensions = kwargs.get("extensions", extensions)
+            window_size = kwargs.get("window_size", window_size)
+
+            metadata = kwargs.get("metadata", metadata)
+            tiny_profile = kwargs.get("tiny_profile", tiny_profile)
+            wait_for_complete_page_load = kwargs.get("wait_for_complete_page_load", wait_for_complete_page_load)
+            lang = kwargs.get("lang", lang)
+            headless = kwargs.get("headless", headless)
+            beep = kwargs.get("beep", beep)
+            close_on_crash = kwargs.get("close_on_crash", close_on_crash)
+            async_queue = kwargs.get("async_queue", async_queue)
+            run_async = kwargs.get("run_async", run_async)
+            profile = kwargs.get("profile", profile)
+            proxy = kwargs.get("proxy", proxy)
+            user_agent = kwargs.get("user_agent", user_agent)
+            reuse_driver = kwargs.get("reuse_driver", reuse_driver)
+            keep_drivers_alive = kwargs.get("keep_drivers_alive", keep_drivers_alive)
+            output = kwargs.get("output", output)
+            output_formats = kwargs.get("output_formats", output_formats)
+            max_retry = kwargs.get("max_retry", max_retry)
+            retry_wait = kwargs.get("retry_wait", retry_wait)
+            create_error_logs = kwargs.get("create_error_logs", create_error_logs)
+
+            raise_exception = kwargs.get("raise_exception", raise_exception)
+            create_driver = kwargs.get("create_driver", create_driver)
+
+            fn_name = func.__name__
+
+            if cache:
+                from .cache import Cache,_get,_has,_get_cache_path,_create_cache_directory_if_not_exists
+
+                _create_cache_directory_if_not_exists(func)
+
+            # # Pool to hold reusable drivers
+            _driver_pool = wrapper_browser._driver_pool if keep_drivers_alive else []
+
+            def run_task(data, is_retry, retry_attempt, retry_driver=None) -> Any:
+                if cache is True:
+                    path = _get_cache_path(func, data)
+                    if _has(path):
+                        return _get(path)
+
+                evaluated_window_size = (
+                    window_size(data) if callable(window_size) else window_size
+                )
+                evaluated_user_agent = (
+                    user_agent(data) if callable(user_agent) else user_agent
+                )
+                evaluated_proxy = evaluate_proxy(proxy(data) if callable(proxy) else proxy)
+                evaluated_profile = profile(data) if callable(profile) else profile
+                evaluated_lang = lang(data) if callable(lang) else lang
+                evaluated_headless = headless(data) if callable(headless) else headless
+                evaluated_extensions = extensions(data) if callable(extensions) else extensions
+
+                if evaluated_profile is not None:
+                    evaluated_profile = str(evaluated_profile)
+
+                if retry_driver is not None:
+                    driver = retry_driver
+                elif reuse_driver and len(_driver_pool) > 0:
+                    driver = _driver_pool.pop()
+                else:
+                    args = []
+                    
+                    if add_arguments:
+                        add_arguments(data, args)
+
+                    driver = Driver(headless=evaluated_headless, proxy=evaluated_proxy, profile=evaluated_profile, tiny_profile=tiny_profile, block_images=block_images, block_images_and_css=block_images_and_css, wait_for_complete_page_load=wait_for_complete_page_load, extensions=evaluated_extensions, arguments=args, user_agent=evaluated_user_agent, window_size=evaluated_window_size, lang=evaluated_lang, beep=beep)
+
+                result = None
+                try:
+                    if max_retry is not None:
+                            driver.config.is_last_retry = not (
+                                (max_retry) > (retry_attempt)
+                            )
+                            driver.config.retry_attempt = retry_attempt
+                            driver.config.is_retry = retry_attempt != 0
+                    # if evaluated_profile is not None:
+                    if "metadata" in kwargs or metadata is not None:
+                        result = func(driver, data, metadata)
+                    else:
+                        result = func(driver, data)
+                    if reuse_driver:
+                        driver.config.is_new = False
+                        _driver_pool.append(driver)  # Add back to the pool for reuse
+                    else:
+                        close_driver(driver)
+
+                    if cache is True or cache == 'REFRESH' :
+                        if is_dont_cache(result):
+                            Cache.remove(func, data)
+                        else:
+                            Cache.put(func, data, result)
+
+                    if is_dont_cache(result):
+                        result = result.data
+
+                    return result
+                except Exception as error:
+                    if isinstance(error, KeyboardInterrupt):
+                        close_driver_pool(_driver_pool)
+                        raise  # Re-raise the KeyboardInterrupt to stop execution
+
+                    if (
+                        must_raise_exceptions
+                        and is_errors_instance(must_raise_exceptions, error)[0]
+                    ):
+                        save_error_logs(format_exc(), driver)
+                        raise
+
+                    if max_retry is not None and (max_retry) > (retry_attempt):
+                        print_exc()
+                        close_driver(driver)
+                        if retry_wait:
+                            print("Waiting for " + str(retry_wait))
+                            sleep(retry_wait)
+                        return run_task(data, True, retry_attempt + 1)
+
+                    if not raise_exception:
+                        print_exc()
+
+                    print("Task failed for input:", data)
+                    if create_error_logs:
+                        save_error_logs(format_exc(), driver)
+                    
+
+                    if not headless:
+                        if not IS_PRODUCTION:
+                            if not close_on_crash:
+                                driver.prompt(
+                                    "We've paused the browser to help you debug. Press 'Enter' to close."
+                                )
+
+                    if reuse_driver:
+                        driver.is_new = False
+                        _driver_pool.append(driver)  # Add back to the pool for reuse
+                    else:
+                        close_driver(driver)
+
+                    if raise_exception:
+                        raise error
+
+                    return result
+
+            number_of_workers = parallel() if callable(parallel) else parallel
+
+            if number_of_workers is not None and not isinstance(number_of_workers, int):
+                raise ValueError("parallel Option must be a number or None")
+
+            used_data = args[0] if len(args) > 0 else data
+            used_data = used_data() if callable(used_data) else used_data
+            orginal_data = used_data
+
+            return_first = False
+            if type(used_data) is not list:
+                return_first = True
+                used_data = [used_data]
+
+            result = []
+            has_number_of_workers = number_of_workers is not None and not (
+                number_of_workers == False
+            )
+
+            if not has_number_of_workers or number_of_workers <= 1:
+                n = 1
+            else:
+                n = min(len(used_data), int(number_of_workers))
+
+            if n <= 1:
+                for index in range(len(used_data)):
+                    data_item = used_data[index]
+                    current_result = run_task(data_item, False, 0)
+                    result.append(current_result)
+            else:
+
+                def run(data_item):
+                    current_result = run_task(data_item, False, 0)
+                    result.append(current_result)
+
+                    return current_result
+
+                if callable(parallel):
+                    print(f"Running {n} Browsers in Parallel")
+                result = run_parallel(run, used_data, n, False)
+
+            if not keep_drivers_alive:
+                close_driver_pool(_driver_pool)
+            if return_first:
+                if not async_queue:
+                    write_output(
+                        output, output_formats, orginal_data, result[0], fn_name
+                    )
+                return result[0]
+            else:
+                if not async_queue:
+                    write_output(output, output_formats, orginal_data, result, fn_name)
+
+                return result
+
+        wrapper_browser._driver_pool = []
+
+        def close_drivers():
+            close_driver_pool(wrapper_browser._driver_pool)
+
+        wrapper_browser.close = close_drivers
+
+        if run_async and async_queue:
+            raise ValueError(
+                "The options 'run_async' and 'async_queue' cannot be applied at the same time. Please set only one of them to True."
+            )
+
+        if run_async:
+
+            @wraps(func)
+            def async_wrapper(*args, **kwargs):
+                def thread_target():
+                    result = wrapper_browser(*args, **kwargs)
+                    async_result.set_result(result)
+
+                thread = Thread(target=thread_target, daemon=True)
+                thread.start()
+                async_result = AsyncResult(thread)
+                return async_result
+
+            async_wrapper._driver_pool = wrapper_browser._driver_pool
+            async_wrapper.close = wrapper_browser.close
+            return async_wrapper
+        elif async_queue:
+
+            @wraps(func)
+            def async_wrapper(**wrapper_kwargs):
+                task_queue = Queue()
+                result_list = []
+                orginal_data = []
+
+                def _worker():
+                    while True:
+                        task = task_queue.get()
+
+                        if task is None:
+                            write_output(
+                                output,
+                                output_formats,
+                                orginal_data,
+                                flatten(result_list),
+                                func.__name__,
+                            )
+                            task_queue.task_done()
+                            break
+
+                        args, kwargs = task
+                        merged_kwargs = {
+                            **wrapper_kwargs,
+                            **kwargs,
+                        }  # Merge wrapper_kwargs with kwargs
+                        if isinstance(args[0], list):
+                            orginal_data.extend(args[0])
+                        else:
+                            orginal_data.append(args[0])
+
+                        result = wrapper_browser(*args, **merged_kwargs)
+
+                        if isinstance(args[0], list):
+                            result_list.extend(result)
+                        else:
+                            result_list.append(result)
+
+                        task_queue.task_done()
+
+                worker_thread = Thread(target=_worker, daemon=True)
+
+                worker_thread.start()
+
+                return AsyncQueueResult(worker_thread, task_queue, result_list)
+
+            async_wrapper._driver_pool = wrapper_browser._driver_pool
+            async_wrapper.close = wrapper_browser.close
+            return async_wrapper
+
+        return wrapper_browser
+
+    if _func is None:
+        return decorator_browser
+    else:
+        return decorator_browser(_func)
---
+++ botasaurus/request_decorator.py
@@ -0,0 +1,289 @@
+from functools import wraps
+from queue import Queue
+from threading import Thread
+from traceback import print_exc, format_exc
+from typing import Any, Callable, Optional, Union, List
+from time import sleep
+from .utils import is_errors_instance
+from .create_request import create_requests
+from .beep_utils import beep_input
+from .list_utils import flatten
+
+from botasaurus.decorators_common import first_run, evaluate_proxy, write_output, IS_PRODUCTION, AsyncQueueResult, AsyncResult,  run_parallel, save_error_logs
+from .dontcache import is_dont_cache
+
+def request(
+    _func: Optional[Callable] = None,
+    *,
+    parallel: Optional[Union[Callable[[Any], int], int]] = None,
+    data: Optional[Union[Callable[[], Any], Any]] = None,
+    metadata: Optional[Any] = None,
+    cache: Union[bool, str] = False,  
+    beep: bool = False,
+    use_stealth: bool = False,
+    run_async: bool = False,
+    async_queue: bool = False,
+    proxy: Optional[Union[Callable[[Any], str], str]] = None,
+    user_agent: Optional[Union[Callable[[Any], str], str]] = None,
+    close_on_crash: bool = False,
+    output: Optional[Union[str, Callable]] = "default",
+    output_formats: Optional[List[str]] = None,
+    raise_exception: bool = False,
+    must_raise_exceptions: Optional[List[Any]] = None,
+    max_retry: Optional[int] = None,
+    retry_wait: Optional[int] = None,
+    create_error_logs: bool = True,
+) -> Callable:
+    def decorator_requests(func: Callable) -> Callable:
+        if not hasattr(func, '_scraper_type'):
+            func._scraper_type = "request"
+
+        @wraps(func)
+        def wrapper_requests(*args, **kwargs) -> Any:
+            global first_run  # Declare the global variable to modify it
+            if first_run:  # Check if it's the first run
+                print("Running")  # If so, print "Running"
+                first_run = False  # Set the flag to False so it doesn't run again
+
+            nonlocal parallel, data, cache, beep, run_async, async_queue, metadata
+            nonlocal proxy, close_on_crash, output, output_formats, max_retry, retry_wait, must_raise_exceptions, raise_exception, create_error_logs
+
+            parallel = kwargs.get("parallel", parallel)
+            data = kwargs.get("data", data)
+            cache = kwargs.get("cache", cache)
+            beep = kwargs.get("beep", beep)
+            run_async = kwargs.get("run_async", run_async)
+            metadata = kwargs.get("metadata", metadata)
+            async_queue = kwargs.get("async_queue", async_queue)
+            proxy = kwargs.get("proxy", proxy)
+            close_on_crash = kwargs.get("close_on_crash", close_on_crash)
+            output = kwargs.get("output", output)
+            output_formats = kwargs.get("output_formats", output_formats)
+            max_retry = kwargs.get("max_retry", max_retry)
+            retry_wait = kwargs.get("retry_wait", retry_wait)
+            must_raise_exceptions = kwargs.get(
+                "must_raise_exceptions", must_raise_exceptions
+            )
+            create_error_logs = kwargs.get("create_error_logs", create_error_logs)
+
+            raise_exception = kwargs.get("raise_exception", raise_exception)
+
+            fn_name = func.__name__
+
+            if cache:
+                from .cache import Cache,_get,_has,_get_cache_path,_create_cache_directory_if_not_exists
+                _create_cache_directory_if_not_exists(func)
+
+            def run_task(
+                data,
+                is_retry,
+                retry_attempt,
+            ) -> Any:
+                if cache is True:
+                    path = _get_cache_path(func, data)
+                    if _has(path):
+                        return _get(path)
+                evaluated_proxy = evaluate_proxy(proxy(data) if callable(proxy) else proxy)
+
+                evaluated_user_agent = (
+                    user_agent(data) if callable(user_agent) else user_agent
+                )
+                reqs = create_requests(
+                    evaluated_proxy, evaluated_user_agent, use_stealth
+                )
+
+                result = None
+                try:
+                    if "metadata" in kwargs or metadata is not None:
+                        result = func(reqs, data, metadata)
+                    else:
+                        result = func(reqs, data)
+                    if cache is True or cache == 'REFRESH' :
+                        if is_dont_cache(result):
+                            Cache.remove(func, data)
+                        else:
+                            Cache.put(func, data, result)
+
+                    if is_dont_cache(result):
+                        result = result.data
+                    return result
+                except Exception as error:
+                    if isinstance(error, KeyboardInterrupt):
+                        raise  # Re-raise the KeyboardInterrupt to stop execution
+
+                    if (
+                        must_raise_exceptions
+                        and is_errors_instance(must_raise_exceptions, error)[0]
+                    ):
+                        save_error_logs(format_exc(), None)
+                        raise
+
+                    if max_retry is not None and (max_retry) > (retry_attempt):
+                        print_exc()
+                        if retry_wait:
+                            print("Waiting for " + str(retry_wait) + " seconds")
+                            sleep(retry_wait)
+                        return run_task(data, True, retry_attempt + 1)
+
+                    if not raise_exception:
+                        print_exc()
+
+                    print("Task failed for input:", data)
+                    if create_error_logs:
+                        save_error_logs(format_exc(), None)
+                    if not IS_PRODUCTION:
+                        if not close_on_crash:
+                            beep_input(
+                                "We've paused the browser to help you debug. Press 'Enter' to close.",
+                                beep,
+                            )
+
+                    
+
+                    if raise_exception:
+                        raise error
+
+                    return result
+
+            number_of_workers = parallel() if callable(parallel) else parallel
+
+            if number_of_workers is not None and not isinstance(number_of_workers, int):
+                raise ValueError("parallel Option must be a number or None")
+
+            used_data = args[0] if len(args) > 0 else data
+            used_data = used_data() if callable(used_data) else used_data
+            orginal_data = used_data
+
+            return_first = False
+            if type(used_data) is not list:
+                return_first = True
+                used_data = [used_data]
+
+            result = []
+
+            has_number_of_workers = number_of_workers is not None and not (
+                number_of_workers == False
+            )
+
+            if not has_number_of_workers or number_of_workers <= 1:
+                n = 1
+            else:
+                n = min(len(used_data), int(number_of_workers))
+
+            if n <= 1:
+                for index in range(len(used_data)):
+                    data_item = used_data[index]
+                    current_result = run_task(data_item, False, 0)
+                    result.append(current_result)
+            else:
+
+                def run(data_item):
+                    current_result = run_task(data_item, False, 0)
+                    result.append(current_result)
+
+                    return current_result
+
+                if callable(parallel):
+                    print(f"Running {n} Requests in Parallel")
+
+                result = run_parallel(run, used_data, n, True)
+
+
+            if return_first:
+                if not async_queue:
+                    write_output(
+                        output, output_formats, orginal_data, result[0], fn_name
+                    )
+                return result[0]
+            else:
+                if not async_queue:
+                    write_output(output, output_formats, orginal_data, result, fn_name)
+
+                return result
+
+        def close():
+            # Stub to not cause errors if user accidentatly changes decorator and calls it.
+            pass
+
+        wrapper_requests.close = close
+        if run_async and async_queue:
+            raise ValueError(
+                "The options 'run_async' and 'async_queue' cannot be applied at the same time. Please set only one of them to True."
+            )
+
+        if run_async:
+
+            @wraps(func)
+            def async_wrapper(*args, **kwargs):
+                def thread_target():
+                    result = wrapper_requests(*args, **kwargs)
+                    async_result.set_result(result)
+
+                thread = Thread(target=thread_target, daemon=True)
+                thread.start()
+                async_result = AsyncResult(thread)
+                async_wrapper.close = wrapper_requests.close
+                return async_result
+
+            return async_wrapper
+
+        elif async_queue:
+
+            @wraps(func)
+            def async_wrapper(*args, **wrapper_kwargs):
+                if args:
+                  raise ValueError('When using "async_queue", data must be passed via ".put".')
+                task_queue = Queue()
+                result_list = []
+                orginal_data = []
+
+                def _worker():
+                    while True:
+                        task = task_queue.get()
+
+                        if task is None:
+                            # Thread Finished
+                            write_output(
+                                output,
+                                output_formats,
+                                orginal_data,
+                                flatten(result_list),
+                                func.__name__,
+                            )
+                            task_queue.task_done()
+                            break
+
+                        args, kwargs = task
+                        merged_kwargs = {
+                            **wrapper_kwargs,
+                            **kwargs,
+                        }  # Merge wrapper_kwargs with kwargs
+
+                        if isinstance(args[0], list):
+                            orginal_data.extend(args[0])
+                        else:
+                            orginal_data.append(args[0])
+
+                        result = wrapper_requests(*args, **merged_kwargs)
+
+                        if isinstance(args[0], list):
+                            result_list.extend(result)
+                        else:
+                            result_list.append(result)
+
+                        task_queue.task_done()
+
+                worker_thread = Thread(target=_worker, daemon=True)
+
+                worker_thread.start()
+                async_wrapper.close = wrapper_requests.close
+                return AsyncQueueResult(worker_thread, task_queue, result_list)
+
+            return async_wrapper
+
+        return wrapper_requests
+
+    if _func is None:
+        return decorator_requests
+    else:
+        return decorator_requests(_func)
