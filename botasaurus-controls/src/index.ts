a46990f0701993ddbc12d8e70fda9bd6aecc2ed0
Chetan <53407137+Chetan11-dev@users.noreply.github.com>
2025-05-10 13:59:45 +0530

    add parsing i controls

--- botasaurus-controls/src/index.ts
+++ botasaurus-controls/src/index.ts
@@ -150,19 +150,24 @@ function convertToBool(isRequired?: boolean | ((data: any) => boolean), data?: a
     return false
   }
 }
-// Define a generic control type with an additional parameter P for custom properties
+// Add this interface near the top with other type definitions
+interface ParseFunction<T> {
+  (value: any): T;
+}
+
+// Update the ControlInput type to include parse
 type ControlInput<V, P = {}> = {
-  label?: string // Label is now optional
+  label?: string
   defaultValue?: V
   helpText?: string
-  isRequired?: boolean | ((data: any) => boolean) // Property to indicate if the control is required
+  isRequired?: boolean | ((data: any) => boolean)
   isDisabled?: (data: any) => boolean
-  disabledMessage?: string,
+  disabledMessage?: string
   isShown?: (data: any) => boolean
   isMetadata?: boolean
   validate?: (value: V, otherData: any) => string | string[] | undefined
-} & P // Merge P with the rest of the control input properties
-
+  parse?: ParseFunction<V> // Add parse function
+} & P
 // Define a generic control type with an additional parameter P for custom properties
 type TextControlInput<V, P = {}> = ControlInput<V, P> & {
   // defaults to true
@@ -228,6 +233,39 @@ function getDefaultCountries() {
   return [{ "value": "AF", "label": "Afghanistan" }, { "value": "AX", "label": "Aland Islands" }, { "value": "AL", "label": "Albania" }, { "value": "DZ", "label": "Algeria" }, { "value": "AS", "label": "American Samoa" }, { "value": "AD", "label": "Andorra" }, { "value": "AO", "label": "Angola" }, { "value": "AI", "label": "Anguilla" }, { "value": "AQ", "label": "Antarctica" }, { "value": "AG", "label": "Antigua And Barbuda" }, { "value": "AR", "label": "Argentina" }, { "value": "AM", "label": "Armenia" }, { "value": "AW", "label": "Aruba" }, { "value": "AU", "label": "Australia" }, { "value": "AT", "label": "Austria" }, { "value": "AZ", "label": "Azerbaijan" }, { "value": "BS", "label": "Bahamas" }, { "value": "BH", "label": "Bahrain" }, { "value": "BD", "label": "Bangladesh" }, { "value": "BB", "label": "Barbados" }, { "value": "BY", "label": "Belarus" }, { "value": "BE", "label": "Belgium" }, { "value": "BZ", "label": "Belize" }, { "value": "BJ", "label": "Benin" }, { "value": "BM", "label": "Bermuda" }, { "value": "BT", "label": "Bhutan" }, { "value": "BO", "label": "Bolivia" }, { "value": "BQ", "label": "Bonaire Saint Eustatius And Saba" }, { "value": "BA", "label": "Bosnia And Herzegovina" }, { "value": "BW", "label": "Botswana" }, { "value": "BV", "label": "Bouvet Island" }, { "value": "BR", "label": "Brazil" }, { "value": "IO", "label": "British Indian Ocean Territory" }, { "value": "VG", "label": "British Virgin Islands" }, { "value": "BN", "label": "Brunei" }, { "value": "BG", "label": "Bulgaria" }, { "value": "BF", "label": "Burkina Faso" }, { "value": "BI", "label": "Burundi" }, { "value": "CV", "label": "Cabo Verde" }, { "value": "KH", "label": "Cambodia" }, { "value": "CM", "label": "Cameroon" }, { "value": "CA", "label": "Canada" }, { "value": "KY", "label": "Cayman Islands" }, { "value": "CF", "label": "Central African Republic" }, { "value": "TD", "label": "Chad" }, { "value": "CL", "label": "Chile" }, { "value": "CN", "label": "China" }, { "value": "CX", "label": "Christmas Island" }, { "value": "CC", "label": "Cocos Islands" }, { "value": "CO", "label": "Colombia" }, { "value": "KM", "label": "Comoros" }, { "value": "CK", "label": "Cook Islands" }, { "value": "CR", "label": "Costa Rica" }, { "value": "HR", "label": "Croatia" }, { "value": "CU", "label": "Cuba" }, { "value": "CW", "label": "Curacao" }, { "value": "CY", "label": "Cyprus" }, { "value": "CZ", "label": "Czechia" }, { "value": "CD", "label": "Democratic Republic Of The Congo" }, { "value": "DK", "label": "Denmark" }, { "value": "DJ", "label": "Djibouti" }, { "value": "DM", "label": "Dominica" }, { "value": "DO", "label": "Dominican Republic" }, { "value": "EC", "label": "Ecuador" }, { "value": "EG", "label": "Egypt" }, { "value": "SV", "label": "El Salvador" }, { "value": "GQ", "label": "Equatorial Guinea" }, { "value": "ER", "label": "Eritrea" }, { "value": "EE", "label": "Estonia" }, { "value": "SZ", "label": "Eswatini" }, { "value": "ET", "label": "Ethiopia" }, { "value": "FK", "label": "Falkland Islands" }, { "value": "FO", "label": "Faroe Islands" }, { "value": "FJ", "label": "Fiji" }, { "value": "FI", "label": "Finland" }, { "value": "FR", "label": "France" }, { "value": "GF", "label": "French Guiana" }, { "value": "PF", "label": "French Polynesia" }, { "value": "TF", "label": "French Southern Territories" }, { "value": "GA", "label": "Gabon" }, { "value": "GM", "label": "Gambia" }, { "value": "GE", "label": "Georgia" }, { "value": "DE", "label": "Germany" }, { "value": "GH", "label": "Ghana" }, { "value": "GI", "label": "Gibraltar" }, { "value": "GR", "label": "Greece" }, { "value": "GL", "label": "Greenland" }, { "value": "GD", "label": "Grenada" }, { "value": "GP", "label": "Guadeloupe" }, { "value": "GU", "label": "Guam" }, { "value": "GT", "label": "Guatemala" }, { "value": "GG", "label": "Guernsey" }, { "value": "GN", "label": "Guinea" }, { "value": "GW", "label": "Guinea Bissau" }, { "value": "GY", "label": "Guyana" }, { "value": "HT", "label": "Haiti" }, { "value": "HM", "label": "Heard Island And Mc Donald Islands" }, { "value": "HN", "label": "Honduras" }, { "value": "HK", "label": "Hong Kong" }, { "value": "HU", "label": "Hungary" }, { "value": "IS", "label": "Iceland" }, { "value": "IN", "label": "India" }, { "value": "ID", "label": "Indonesia" }, { "value": "IR", "label": "Iran" }, { "value": "IQ", "label": "Iraq" }, { "value": "IE", "label": "Ireland" }, { "value": "IM", "label": "Isle Of Man" }, { "value": "IL", "label": "Israel" }, { "value": "IT", "label": "Italy" }, { "value": "CI", "label": "Ivory Coast" }, { "value": "JM", "label": "Jamaica" }, { "value": "JP", "label": "Japan" }, { "value": "JE", "label": "Jersey" }, { "value": "JO", "label": "Jordan" }, { "value": "KZ", "label": "Kazakhstan" }, { "value": "KE", "label": "Kenya" }, { "value": "KI", "label": "Kiribati" }, { "value": "XK", "label": "Kosovo" }, { "value": "KW", "label": "Kuwait" }, { "value": "KG", "label": "Kyrgyzstan" }, { "value": "LA", "label": "Laos" }, { "value": "LV", "label": "Latvia" }, { "value": "LB", "label": "Lebanon" }, { "value": "LS", "label": "Lesotho" }, { "value": "LR", "label": "Liberia" }, { "value": "LY", "label": "Libya" }, { "value": "LI", "label": "Liechtenstein" }, { "value": "LT", "label": "Lithuania" }, { "value": "LU", "label": "Luxembourg" }, { "value": "MO", "label": "Macao" }, { "value": "MG", "label": "Madagascar" }, { "value": "MW", "label": "Malawi" }, { "value": "MY", "label": "Malaysia" }, { "value": "MV", "label": "Maldives" }, { "value": "ML", "label": "Mali" }, { "value": "MT", "label": "Malta" }, { "value": "MH", "label": "Marshall Islands" }, { "value": "MQ", "label": "Martinique" }, { "value": "MR", "label": "Mauritania" }, { "value": "MU", "label": "Mauritius" }, { "value": "YT", "label": "Mayotte" }, { "value": "MX", "label": "Mexico" }, { "value": "FM", "label": "Micronesia" }, { "value": "MD", "label": "Moldova" }, { "value": "MC", "label": "Monaco" }, { "value": "MN", "label": "Mongolia" }, { "value": "ME", "label": "Montenegro" }, { "value": "MS", "label": "Montserrat" }, { "value": "MA", "label": "Morocco" }, { "value": "MZ", "label": "Mozambique" }, { "value": "MM", "label": "Myanmar" }, { "value": "NA", "label": "Namibia" }, { "value": "NR", "label": "Nauru" }, { "value": "NP", "label": "Nepal" }, { "value": "NL", "label": "Netherlands" }, { "value": "AN", "label": "Netherlands Antilles" }, { "value": "NC", "label": "New Caledonia" }, { "value": "NZ", "label": "New Zealand" }, { "value": "NI", "label": "Nicaragua" }, { "value": "NE", "label": "Niger" }, { "value": "NG", "label": "Nigeria" }, { "value": "NU", "label": "Niue" }, { "value": "NF", "label": "Norfolk Island" }, { "value": "MP", "label": "Northern Mariana Islands" }, { "value": "KP", "label": "North Korea" }, { "value": "MK", "label": "North Macedonia" }, { "value": "NO", "label": "Norway" }, { "value": "OM", "label": "Oman" }, { "value": "PK", "label": "Pakistan" }, { "value": "PW", "label": "Palau" }, { "value": "PS", "label": "Palestinian Territory" }, { "value": "PA", "label": "Panama" }, { "value": "PG", "label": "Papua New Guinea" }, { "value": "PY", "label": "Paraguay" }, { "value": "PE", "label": "Peru" }, { "value": "PH", "label": "Philippines" }, { "value": "PN", "label": "Pitcairn" }, { "value": "PL", "label": "Poland" }, { "value": "PT", "label": "Portugal" }, { "value": "PR", "label": "Puerto Rico" }, { "value": "QA", "label": "Qatar" }, { "value": "CG", "label": "Republic Of The Congo" }, { "value": "RE", "label": "Reunion" }, { "value": "RO", "label": "Romania" }, { "value": "RU", "label": "Russia" }, { "value": "RW", "label": "Rwanda" }, { "value": "BL", "label": "Saint Barthelemy" }, { "value": "SH", "label": "Saint Helena" }, { "value": "KN", "label": "Saint Kitts And Nevis" }, { "value": "LC", "label": "Saint Lucia" }, { "value": "MF", "label": "Saint Martin" }, { "value": "PM", "label": "Saint Pierre And Miquelon" }, { "value": "VC", "label": "Saint Vincent And The Grenadines" }, { "value": "WS", "label": "Samoa" }, { "value": "SM", "label": "San Marino" }, { "value": "ST", "label": "Sao Tome And Principe" }, { "value": "SA", "label": "Saudi Arabia" }, { "value": "SN", "label": "Senegal" }, { "value": "RS", "label": "Serbia" }, { "value": "CS", "label": "Serbia And Montenegro" }, { "value": "SC", "label": "Seychelles" }, { "value": "SL", "label": "Sierra Leone" }, { "value": "SG", "label": "Singapore" }, { "value": "SX", "label": "Sint Maarten" }, { "value": "SK", "label": "Slovakia" }, { "value": "SI", "label": "Slovenia" }, { "value": "SB", "label": "Solomon Islands" }, { "value": "SO", "label": "Somalia" }, { "value": "ZA", "label": "South Africa" }, { "value": "GS", "label": "South Georgia And The South Sandwich Islands" }, { "value": "KR", "label": "South Korea" }, { "value": "SS", "label": "South Sudan" }, { "value": "ES", "label": "Spain" }, { "value": "LK", "label": "Sri Lanka" }, { "value": "SD", "label": "Sudan" }, { "value": "SR", "label": "Suriname" }, { "value": "SJ", "label": "Svalbard And Jan Mayen" }, { "value": "SE", "label": "Sweden" }, { "value": "CH", "label": "Switzerland" }, { "value": "SY", "label": "Syria" }, { "value": "TW", "label": "Taiwan" }, { "value": "TJ", "label": "Tajikistan" }, { "value": "TZ", "label": "Tanzania" }, { "value": "TH", "label": "Thailand" }, { "value": "TL", "label": "Timor Leste" }, { "value": "TG", "label": "Togo" }, { "value": "TK", "label": "Tokelau" }, { "value": "TO", "label": "Tonga" }, { "value": "TT", "label": "Trinidad And Tobago" }, { "value": "TN", "label": "Tunisia" }, { "value": "TR", "label": "Turkey" }, { "value": "TM", "label": "Turkmenistan" }, { "value": "TC", "label": "Turks And Caicos Islands" }, { "value": "TV", "label": "Tuvalu" }, { "value": "UG", "label": "Uganda" }, { "value": "UA", "label": "Ukraine" }, { "value": "AE", "label": "United Arab Emirates" }, { "value": "GB", "label": "United Kingdom" }, { "value": "US", "label": "United States" }, { "value": "UM", "label": "United States Minor Outlying Islands" }, { "value": "UY", "label": "Uruguay" }, { "value": "VI", "label": "U S Virgin Islands" }, { "value": "UZ", "label": "Uzbekistan" }, { "value": "VU", "label": "Vanuatu" }, { "value": "VA", "label": "Vatican" }, { "value": "VE", "label": "Venezuela" }, { "value": "VN", "label": "Vietnam" }, { "value": "WF", "label": "Wallis And Futuna" }, { "value": "EH", "label": "Western Sahara" }, { "value": "YE", "label": "Yemen" }, { "value": "ZM", "label": "Zambia" }, { "value": "ZW", "label": "Zimbabwe" }]
 }
 type SectionControls = Omit<Controls, "section">
+
+const negativeValues = new Set(["false", "no", "n", "0", "nah", "nope", "never", "negative", "f"]);
+const affirmativeValues = new Set(["true", "yes", "y", "1", "yeah", "yep", "sure", "ok", "okay", "affirmative", "t"]);
+
+const parseBoolean = (value:any) => {
+  if (typeof value === 'string') {
+
+        const normalizedValue = value.trim().toLowerCase();
+        if (negativeValues.has(normalizedValue)) {
+          return false;
+        }
+        if (affirmativeValues.has(normalizedValue)) {
+          return true;
+        }
+  }
+  return value;
+}
+const parseNumber = (value:any)=>{
+  if (typeof value === 'string') {
+      const parsed = parseInt(value, 10);
+      if (!isNaN(parsed)) {
+          return parsed;
+      }
+  }
+  return value;
+}
+
+const parseListOfTexts = (value:any)=>{
+  if (value === null) {
+      return []
+  }
+  return value;
+}
 
 class Controls {
   private isSectionControl = false;
@@ -368,13 +406,14 @@ class Controls {
     this.add<string[]>(id, "listOfTexts", {
       ...props,
       limit,
+      parse:parseListOfTexts,
       defaultValue,
     })
 
     return this
   }
   number(id: string, props: NumberControlInput<number> = {}) {
-    return this.add<number>(id, "number", { defaultValue: null as any, ...props })
+    return this.add<number>(id, "number", { defaultValue: null as any, parse:parseNumber, ...props})
   }
 
   // Method to add a number control that must be greater than or equal to zero
@@ -390,11 +429,11 @@ class Controls {
   }
 
   switch(id: string, props: Omit<ControlInput<boolean>, "required"> = {}) {
-    return this.add<boolean>(id, "switch", { defaultValue: false, ...props })
+    return this.add<boolean>(id, "switch", { defaultValue: false,parse:parseBoolean, ...props })
   }
 
   checkbox(id: string, props: Omit<ControlInput<boolean>, "required"> = {}) {
-    return this.add<boolean>(id, "checkbox", { defaultValue: false, ...props })
+    return this.add<boolean>(id, "checkbox", { defaultValue: false,parse:parseBoolean,  ...props })
   }
 
   textarea(id: string, props: TextControlInput<string> = {}) {
@@ -413,6 +452,7 @@ class Controls {
     this.add<string[]>(id, "listOfLinks", {
       ...props,
       limit,
+      parse:parseListOfTexts,
       defaultValue, // Ensure default value is [""] if not provided
     })
 
@@ -529,20 +569,54 @@ class Controls {
     return this.defaultData
   }
 
+
+// Add this helper method to the Controls class
+private parse(data: any) {
+  const parsedData: { [key: string]: any } = {};
+  
+  this.iterateControls((control) => {
+    const value = data[control.id];
+    if (control.parse) {
+      try {
+        parsedData[control.id] = control.parse(value);
+      } catch (error) {
+        // If parsing fails, keep original value
+        parsedData[control.id] = value;
+      }
+    } else {
+      parsedData[control.id] = value;
+    }
+  });
+  
+  return parsedData;
+}
   //@ts-ignore
-  private getBackendValidationResult(data: any) {
-    const defaultData = this.getDefaultData()
+  private getBackendValidationResult(data: any, tryParsing = false) {
     const mergedData: { [key: string]: any } = {}
-
     // Merge provided data with default data, picking only the keys that correspond to defined controls
-    this.iterateControls((control) => {
-      const controlId = control.id
-      mergedData[controlId] = data.hasOwnProperty(controlId)
-        ? data[controlId]
-        : defaultData[controlId]
-    })
+    if (tryParsing) {
+      this.iterateControls((control) => {
+        const controlId = control.id
+        mergedData[controlId] = data.hasOwnProperty(controlId)
+          ? data[controlId]
+          : null
+      })
+    } else {
+      const defaultData = this.getDefaultData()
+      this.iterateControls((control) => {
+        const controlId = control.id
+        mergedData[controlId] = data.hasOwnProperty(controlId)
+          ? data[controlId]
+          : defaultData[controlId]
+      })
+    }
 
-    const errors = this.validate(mergedData) // Validate the merged data
+    let parsedData = mergedData
+    if (tryParsing) {
+      parsedData = this.parse(mergedData)
+    }
+
+    const errors = this.validate(parsedData) // Validate the merged data
 
     if (!isEmptyObject(errors)) {
       return {
@@ -557,25 +631,23 @@ class Controls {
 
     this.iterateControls((control) => {
       const controlId = control.id
-      let value = mergedData[controlId]
+      let value = parsedData[controlId]
 
-
-      if (control.type === "text"  || control.type === "textarea" ||control.type === "search") {
+      if (control.type === "text" || control.type === "textarea" || control.type === "search") {
         if (canTrim(control)) {
           value = value.trim()
         }
       }
       else if (control.type === "listOfTexts") {
-
         value = value.filter(isNotEmpty)
         // @ts-ignore
         if (canTrim(control)) {
-          value = value.map((x:any) => x.trim())
+          value = value.map((x: any) => x.trim())
         }
       }
       // Filter valid URLs for listOfLinks
       else if (control.type === "listOfLinks") {
-        value = value.filter(isNotEmpty).map(getLink).filter((x:any)=>x!== null).map((x:any)=>x.trim())
+        value = value.filter(isNotEmpty).map(getLink).filter((x: any) => x !== null).map((x: any) => x.trim())
       }
 
       if (control.isMetadata) {
