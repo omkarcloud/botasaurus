8cfd6b47581aad987fc6290452b74d465a163b81
Chetan Jain <53407137+Chetan11-dev@users.noreply.github.com>
2025-07-24 20:22:33 +0530

    Update install-bota-desktop.sh

--- vm-scripts/install-bota-desktop.sh
+++ vm-scripts/install-bota-desktop.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt install apache2
 # Install required packages and dependencies
-sudo apt install -y python3-pip # see rm works
+sudo apt install -y apache2 python3-pip # see rm works
 python3 -m pip install --break-system-packages --upgrade bota
 alias python=python3
 echo "alias python=python3" >> ~/.bashrc
