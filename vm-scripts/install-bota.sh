919f4cea366d905e3820ea4cbbb7e16ecf519e2c
Chetan <53407137+Chetan11-dev@users.noreply.github.com>
2025-07-29 17:36:15 +0530

    fixes

--- vm-scripts/install-bota.sh
+++ vm-scripts/install-bota.sh
@@ -3,8 +3,8 @@
 sudo apt-get update
 # Install required packages and dependencies
 sudo apt install -y python3-pip
-python3 -m pip install --upgrade bota
-alias python=python3
+python3 -m pip install --break-system-packages --upgrade bota
+
 echo "alias python=python3" >> ~/.bashrc
 # Temporary fix due for this bug, https://stackoverflow.com/questions/79196464/react-js-failed-to-compile-unexpected-end-of-json-input-error, in next 1 year, on new node version release, remove this code 
 sudo npm install -g n
